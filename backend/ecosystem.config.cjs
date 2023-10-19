92edb4f6bf8f6c73936f01747b842e242703dff4
Dmitry Rusinov <kachek96@yandex.ru>
2023-10-19 17:29:43 +0300

    change IP

---
+++ backend/ecosystem.config.cjs
@@ -0,0 +1,21 @@
+module.exports = {
+  apps: [
+    {
+      name: "mesto-auto-deploy",
+      script: "./app.js",
+    },
+  ],
+
+  // Deployment Configuration
+  deploy: {
+    production: {
+      user: "mesto-rusinov",
+      host: "51.250.8.9",
+      ref: "origin/main",
+      repo: "git@github.com:Dmitry-Rusinov/react-mesto-api-full-gha.git",
+      path: "/home/mesto_rusinov/auto-deploy",
+      "pre-deploy-local": "scp .env mesto_rusinov@51.250.8.9:/home/mesto_rusinov/auto-deploy/current/backend",
+      "post-deploy": "pwd && cd backend && npm i && pm2 startOrRestart ecosystem.config.cjs --env production",
+    },
+  },
+};
